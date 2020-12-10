622cd50f4480abbca3b9e56387e06356334af144
Tyler Weinacker <tweinacker57@gmail.com>
2020-12-09 21:40:23 -0500

    fix quotes in last commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from “vue”;
-import App from “./App.vue”;
-import router from “./router”;
-import axios from “axios”;
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 axios.defaults.baseURL =
-  process.env.NODE_ENV === “development” ?“http://localhost:3000” : “/”;
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 Vue.config.productionTip = false;
 new Vue({
   router,
   render: h => h(App)
-}).$mount(“#app”);
+}).$mount("#app");
